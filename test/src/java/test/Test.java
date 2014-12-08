6430624a025e7f905504dd26f4d0c00483e2cec7
BBC300041 <BBC300041@E27726.cmjgwd.canon-mj.co.jp>
2014-12-08 11:40:20 +0900

    java Commit

---
+++ test/src/java/test/Test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author BBC300041
+ */
+public class Test {
+    
+}
